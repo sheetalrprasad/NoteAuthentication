3c39a9bdc276febff0e5773c3978176e33ce3d8e
sheetalrprasad <sheetalraniprasad@gmail.com>
2021-08-28 16:58:22 +0530

    ui using flasgger

--- main.py
+++ main.py
@@ -2,18 +2,47 @@ from flask import Flask, request
 import pandas as pd
 import numpy as np
 import pickle
+import flasgger
+from flasgger import Swagger
 
 app=Flask(__name__)
+Swagger(app)
 
 pickle_in = open('classifier.pkl','rb')
 classifier = pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
-    return "Welcome All..."
+    return "Welcome All... 0 = Authentic and 1 = Fake"
 
 @app.route('/predict')
 def predict_note_authentication():
+
+    """Let's authenticate the bank note
+    This is using docstring for specifications.
+    ---
+    parameters:
+        - name: variance
+          in: query
+          type: number
+          required: true
+        - name: skewness
+          in: query
+          type: number
+          required: true
+        - name: curtosis
+          in: query
+          type: number
+          required: true
+        - name: entropy
+          in: query
+          type: number
+          required: true
+    responses:
+        200:
+            description: The output values
+
+    """
     variance = request.args.get('variance')
     skewness = request.args.get('skewness')
     curtosis = request.args.get('curtosis')
@@ -24,6 +53,20 @@ def predict_note_authentication():
 
 @app.route('/predict_file',methods=['POST'])
 def predict_note_file():
+    """Let's Authenticate Bank Note
+    This is using docstring for specifiations.
+    ---
+    parameters:
+        - name: file
+          in: formData
+          type: file
+          required: true
+
+    responses:
+        200:
+            description: The output values
+    
+    """
     df_test = pd.read_csv(request.files.get("file"))
     prediction = classifier.predict(df_test)
     return "The predicted values for the csv is: " + str(list(prediction))
